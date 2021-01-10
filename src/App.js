4699e8c501def0ac5e3024dbd25e0593f436a7ca
Krona Emmanuel <kronaemmanuel@gmail.com>
2021-01-10 21:54:07 +0500

    Day 3 Concluded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from "./components/TodoItem";
 import data from "./data";
 
-function App() {
-  let items = data.map((item) => {
-    return (
-      <TodoItem text={item.text} completed={item.completed} key={item.id} />
-    );
-  });
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoData: data,
+    };
 
-  return <div className="todolist-container">{items}</div>;
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?userId=1")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          todoData: json,
+        });
+      });
+  }
+
+  toggleCompleted(id) {
+    console.log("Toggled ", id);
+    this.setState((prevState) => {
+      const updatedState = prevState.todoData.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      });
+
+      return { todoData: updatedState };
+    });
+  }
+
+  render() {
+    let items = this.state.todoData.map((item) => {
+      return (
+        <TodoItem
+          text={item.title}
+          toggleCompleted={this.toggleCompleted}
+          completed={item.completed}
+          key={item.id}
+          todoId={item.id}
+        />
+      );
+    });
+
+    return <div className="todolist-container">{items}</div>;
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 function TodoItem(props) {
+  const completedStyles = { textDecoration: "line-through" };
   return (
     <div className="todoitem">
-      <input type="checkbox" checked={props.completed}></input>
-      <label>{props.text}</label>
+      <input
+        type="checkbox"
+        onChange={() => props.toggleCompleted(props.todoId)}
+        checked={props.completed}
+      ></input>
+      <label style={props.completed ? completedStyles : null}>
+        {props.text}
+      </label>
     </div>
   );
 }
